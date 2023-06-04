dc4ee024b9f05a5227cf0f793088502c1b7580cb
Chandu <chandur659@.com>
2023-06-03 21:48:31 -0500

    fullstack app

--- Fullstack/app.js
+++ Fullstack/app.js
@@ -1,30 +1,20 @@
 let express = require('express');
 let app = express();
+let categoryRouter = require('./src/controllers/categoryRouter.js');
+let productRouter = require('./src/controllers/productRouter.js');
+
+
 let port = 8812;
 
 app.get('/',function(req,res){
     res.send('Hi From Express Default Route')
 })
 
-app.get('/category',function(req,res){
-    res.send('This is Catgeory route')
-})
-
-app.get('/details',function(req,res){
-    res.send('This is category Details')
-})
-
-
-app.get('/products',function(req,res){
-    res.send('This is Product route')
-})
-
-app.get('/details',function(req,res){
-    res.send('This is product Details')
-})
+app.use('/category',categoryRouter)
+app.use('/products',productRouter)
 
 //create server
 app.listen(port,function(err){
-    if(err) throw err;
+    if(err) throw err
     console.log(`Server is running on port ${port}`)
 })
