ae1ca1935c15ecb9ad1ee4815a16b9b656fe3ee6
chandu659 <chandur659@gmail.com>
2024-01-01 14:54:14 -0600

    Graph QL API

--- graphQL/app.js
+++ graphQL/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const port = 8600;
-const app = express;
-const axios = require('axios');
+const app = express();
 const {graphqlHTTP} = require('express-graphql');
-const schema = require('./schema/schema');
+const schema = require('./Schema/schema');
+
+app.use('/graphql',
+    graphqlHTTP({
+        schema: schema,
+        graphiql:true
+    })
+)
+
+app.listen(port,() => {
+    console.log(`listening on port ${port}`)
+})
